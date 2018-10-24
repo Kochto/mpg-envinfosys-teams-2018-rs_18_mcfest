fa6404eaedc7a37cbc67817029cccd7c7471dcc4
eikeschott <eikeschott0@gmail.com>
2018-10-24 13:39:54 +0200

    Update src/000_setup.R

---
+++ src/000_setup.R
@@ -0,0 +1,27 @@
+libs = c("link2GI",
+         "raster",
+         "rgdal",
+         "sp")
+lapply(libs, require, character.only = TRUE)
+
+# Set pathes -------------------------------------------------------------------
+# Automatically set root direcory depending on booted system
+if(Sys.info()["sysname"] == "Windows"){
+  filepath_base = "F:/09_Semester/remote_sensing"
+} else {
+  filepath_base = "/media/memory/permanent/edu/remote_sensing"
+}
+
+# Set project specific subfolders
+project_folders = c("data/",                                 # data folders
+                    "data/aerial/", "data/lidar/", "data/grass/", 
+                    "data/data_mof", "data/tmp/", 
+                    "run/", "log/",                          # bins and logging
+                    "mpg-envinfosys-teams-2018-rs_18_mcfest/src/",   # source code
+                    "mpg-envinfosys-teams-2018-rs_18_mcfest/doc/")   # markdown etc. 
+
+envrmt = initProj(projRootDir = filepath_base, GRASSlocation = "data/grass",
+                  projFolders = project_folders, path_prefix = "path_", 
+                  global = FALSE)
+
+rasterOptions(tmpdir = envrmt$path_data_tmp)

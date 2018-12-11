ce3547a96d500e34c3b1fa9987db6ea14844c58c
eikeschott <eikeschott0@gmail.com>
2018-12-11 20:24:15 +0100

    poster plots

--- src/000_setup.R
+++ src/000_setup.R
@@ -2,7 +2,7 @@ root_folder <- envimaR::alternativeEnvi(root_folder = "~/edu/mpg-envinsys-plygrn
                                  alt_env_value = "PCRZP", alt_env_root_folder = "F:\\edu\\mpg-envinsys-plygrnd")
 
 #Set librarys
-libs = c("lidR", "link2GI", "mapview", "raster", "rgdal", 
+libs = c("lidR", "link2GI", "mapview", "raster", "rgdal", "ForestTools",
          "sp", "corrplot", "RStoolbox", "glcm", "TileManager", "factoextra", "smoothie")
 lapply(libs, require, character.only = TRUE)
 
